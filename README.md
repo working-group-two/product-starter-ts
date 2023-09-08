5cde59cd7e50caeb5aafeb9c091a2f421399458b
Gunnar Inge G. Sortland <gi@wgtwo.com>
2023-09-08 10:34:29 +0200

    Use buf registry

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # product-starter-ts
 
+Our API is published at the [Buf Schema Registry > wgtwo](https://buf.build/wgtwo).
+
+This is currently experimental, and we are looking for feedback.
+
 ## Build typescript from proto
 ```
-npm i
-cd proto
-npx buf generate
+npm install
 ```
 
 ## Run project
-```
+
+```shell
 npx tsx ./src/client/main.ts
 ```
--- src/client/main.ts
+++ src/client/main.ts
@@ -1,51 +1,63 @@
-import {
-    SendTextToSubscriberRequest,
-    SendMessageResponse
-} from "../proto/wgtwo/sms/v1/sms_pb";
+import {SendTextFromSubscriberRequest} from "@buf/wgtwo_wgtwoapis.community_timostamm-protobuf-ts/wgtwo/sms/v1/sms_pb.d"
+import {CallCredentials, ChannelCredentials, credentials} from "@grpc/grpc-js";
+import {SmsServiceClient} from "@buf/wgtwo_wgtwoapis.community_timostamm-protobuf-ts/wgtwo/sms/v1/sms_pb.client";
+import {GrpcTransport} from '@protobuf-ts/grpc-transport';
+import {Metadata} from "@grpc/grpc-js/src/metadata";
+import {CallMetadataOptions} from "@grpc/grpc-js/src/call-credentials";
+import {OAuth2Client, OAuth2Fetch} from '@badgateway/oauth2-client';
 
-import {
-    Client,
-    credentials
-} from "@grpc/grpc-js";
+async function main() {
+    const sendFrom = process.env.SEND_FROM as string
+    const sendTo = process.env.SEND_TO as string
 
-(async function main() {
-    const smsRequest = new SendTextToSubscriberRequest();
-    smsRequest.content = "Hello, this is your SMS content.";
-    smsRequest.toSubscriber = "+1234567890"; // The recipient's international phone number
-    smsRequest.fromAddress = "YourSenderID";   // Your sender ID
 
-    try {
-        const response = await sendSMS(smsRequest);
-        console.log("SMS sent:", response);
-    } catch (error) {
-        console.error("Error sending SMS:", error);
+    const oAuth2Client = new OAuth2Client({
+        server: 'https://id.wgtwo.com/',
+        clientId: process.env.CLIENT_ID as string,
+        clientSecret: process.env.CLIENT_SECRET as string,
+        tokenEndpoint: '/oauth2/token',
+    });
+
+    const tokenSupplier = new OAuth2Fetch({
+        client: oAuth2Client,
+        getNewToken: async () => {
+            console.log("Fetching new OAuth 2.0 client credentials token")
+            return oAuth2Client.clientCredentials({
+                scope: ['sms.text:send_from_subscriber'],
+            });
+        }
+    })
+
+    const token = await tokenSupplier.getAccessToken()
+    console.log(`Token ${token}`)
+
+    async function metadataGenerator(options: CallMetadataOptions, callback: (err: Error | null, metadata: Metadata) => void) {
+        const metadata = new Metadata();
+        const accessToken = await tokenSupplier.getAccessToken()
+        metadata.add('Authorization', "Bearer " + accessToken)
+        callback(null, metadata);
     }
-})()
 
-async function sendSMS(smsRequest: SendTextToSubscriberRequest): Promise<SendMessageResponse> {
-    const client = new Client("sandbox.api.wgtwo.com:443", credentials.createSsl());
+    const transport = new GrpcTransport({
+        host: "api.wgtwo.com:443",
+        channelCredentials: credentials.combineChannelCredentials(
+            ChannelCredentials.createSsl(),
+            CallCredentials.createFromMetadataGenerator(metadataGenerator),
+        ),
+    });
 
-    try {
-        const serialize = (request: SendTextToSubscriberRequest) => Buffer.from(JSON.stringify(request));
-        const deserialize = (response: Buffer) => JSON.parse(response.toString());
-        return new Promise((resolve, reject) => {
-            client.makeUnaryRequest(
-                "/wgtwo.sms.v1.SmsService/SendTextToSubscriber",
-                serialize,
-                deserialize,
-                smsRequest,
-                (err, value) => {
-                    client.close();
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve(value);
-                    }
-                }
-            );
-        });
-    } catch (error) {
-        client.close();
-        throw error;
+
+    // Create a SMS client
+    const stub: SmsServiceClient = new SmsServiceClient(transport);
+
+    const request: SendTextFromSubscriberRequest = {
+        content: "Hello, world",
+        toAddress: sendTo,
+        fromSubscriber: sendFrom,
     }
+
+    const call = await stub.sendTextFromSubscriber(request, {timeout: 1000})
+    console.log(call.response.messageId)
 }
+
+void main();
