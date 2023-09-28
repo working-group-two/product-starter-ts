1656fe2a34ddd21c1c7e1b0683c18d8312de7a17
Gunnar Inge Gjøvik Sortland <gi@wgtwo.com>
2023-09-28 17:49:11 +0200

    Enable pre-commit hooks using eslint (#5)

--- src/client/consentEvents.ts
+++ src/client/consentEvents.ts
@@ -1,59 +1,72 @@
-import {GrpcTransport} from "@protobuf-ts/grpc-transport";
-import {ConsentEventServiceClient} from "@buf/wgtwo_wgtwoapis.community_timostamm-protobuf-ts/wgtwo/consents/v1/consent_events_pb.client";
-import {StreamConsentChangeEventsRequest, StreamConsentChangeEventsResponse} from "@buf/wgtwo_wgtwoapis.community_timostamm-protobuf-ts/wgtwo/consents/v1/consent_events_pb";
-import {AckStatus_StatusCode} from "@buf/wgtwo_wgtwoapis.community_timostamm-protobuf-ts/wgtwo/events/v1/events_pb";
+import { type GrpcTransport } from '@protobuf-ts/grpc-transport'
+import {
+  ConsentEventServiceClient
+} from '@buf/wgtwo_wgtwoapis.community_timostamm-protobuf-ts/wgtwo/consents/v1/consent_events_pb.client'
+import {
+  type StreamConsentChangeEventsRequest,
+  type StreamConsentChangeEventsResponse
+} from '@buf/wgtwo_wgtwoapis.community_timostamm-protobuf-ts/wgtwo/consents/v1/consent_events_pb'
+import { AckStatus_StatusCode } from '@buf/wgtwo_wgtwoapis.community_timostamm-protobuf-ts/wgtwo/events/v1/events_pb'
 
-import {LOG} from "./logger";
+import { LOG } from './logger'
 
 export class ConsentEvents {
-    private readonly client: ConsentEventServiceClient;
+  private readonly client: ConsentEventServiceClient
+  private readonly callback: (response: StreamConsentChangeEventsResponse) => Promise<void>
 
-    constructor(transport: GrpcTransport) {
-        this.client = new ConsentEventServiceClient(transport);
+  constructor (transport: GrpcTransport, callback: (response: StreamConsentChangeEventsResponse) => Promise<void>) {
+    this.client = new ConsentEventServiceClient(transport)
+    this.callback = callback
+  }
+
+  async ackConsentEvent (response: StreamConsentChangeEventsResponse): Promise<void> {
+    const request = {
+      ackInfo: response.metadata?.ackInfo
     }
+    const call = await this.client.ackConsentChangeEvent(request)
+    const status = name(call.response.ackStatus?.statusCode)
+    LOG.info(`Ack status: ${status}`)
+  }
 
-    async ackConsentEvent(response: StreamConsentChangeEventsResponse) {
-        let request = {
-            ackInfo: response!!.metadata!!.ackInfo,
-        };
-        const call = await this.client.ackConsentChangeEvent(request);
-        const status = name(call.response.ackStatus?.statusCode)
-        LOG.info(`Ack status: ${status}`);
-    }
-
-    async stream({request, callback}: {
-        request: StreamConsentChangeEventsRequest,
-        callback: (response: StreamConsentChangeEventsResponse) => Promise<void>
-    }) {
-        while (true) {
-            LOG.info("Starting new stream")
-            const call = this.client.streamConsentChangeEvents(request);
+  async stream ({ request }: {
+    request: StreamConsentChangeEventsRequest
+  }): Promise<void> {
+    while (true) {
+      LOG.info('Starting new stream')
+      const call = this.client.streamConsentChangeEvents(request)
+      try {
+        LOG.debug('Waiting for consent events')
+        call.responses.onMessage((response) => {
+          void (async () => {
             try {
-                LOG.debug("Waiting for consent events")
-                call.responses.onMessage(async (response) => {
-                    await callback(response);
-                    await this.ackConsentEvent(response);
-                });
-                await call;
+              await this.callback(response)
+              await this.ackConsentEvent(response)
             } catch (e) {
-                // @ts-ignore
-                const grpcStatusCode = e.code;
-                switch (grpcStatusCode) {
-                    case "UNAVAILABLE":
-                    case "DEADLINE_EXCEEDED":
-                        LOG.info(`Disconnected, reconnecting in 100ms ${grpcStatusCode}`)
-                        await new Promise(resolve => setTimeout(resolve, 5000));
-                        break;
-                    default:
-                        LOG.info(`Disconnected, reconnecting in 10s: ${grpcStatusCode}`)
-                        await new Promise(resolve => setTimeout(resolve, 10000));
-                        break;
-                }
+              LOG.info(`Error handling consent event: ${String(e)}`)
             }
+          })()
+        })
+
+        await call
+      } catch (e) {
+        // @ts-expect-error Error type is not exported
+        const grpcStatusCode = e.code
+        switch (grpcStatusCode) {
+          case 'UNAVAILABLE':
+          case 'DEADLINE_EXCEEDED':
+            LOG.info(`Disconnected, reconnecting in 100ms ${grpcStatusCode}`)
+            await new Promise(resolve => setTimeout(resolve, 5000))
+            break
+          default:
+            LOG.info(`Disconnected, reconnecting in 10s: ${grpcStatusCode}`)
+            await new Promise(resolve => setTimeout(resolve, 10000))
+            break
         }
+      }
     }
+  }
 }
 
-function name(value: AckStatus_StatusCode | undefined) {
-    return value === undefined ? AckStatus_StatusCode.UNSPECIFIED : AckStatus_StatusCode[value];
+function name (value: AckStatus_StatusCode | undefined): string {
+  return value === undefined ? AckStatus_StatusCode[AckStatus_StatusCode.UNSPECIFIED] : AckStatus_StatusCode[value]
 }
--- src/client/logger.ts
+++ src/client/logger.ts
@@ -1,32 +1,38 @@
 type LogLevel = 'WARN' | 'INFO' | 'DEBUG'
 
 class Logger {
-    private readonly currentLogLevel: LogLevel;
+  private readonly currentLogLevel: LogLevel
 
-    constructor(logLevel: LogLevel) {
-        this.currentLogLevel = logLevel;
-    }
+  constructor (logLevel: LogLevel) {
+    this.currentLogLevel = logLevel
+  }
 
-    debug(...messages: any[]) {
-        this.print('INFO', ...messages);
+  debug (...messages: any[]): void {
+    if (this.shouldLog('DEBUG')) {
+      this.print('INFO', ...messages)
     }
+  }
 
-    info(...messages: any[]) {
-        this.print('INFO', ...messages);
+  info (...messages: any[]): void {
+    if (this.shouldLog('INFO')) {
+      this.print('INFO', ...messages)
     }
+  }
 
-    warn(...messages: any[]) {
-        this.print('WARN', ...messages);
+  warn (...messages: any[]): void {
+    if (this.shouldLog('WARN')) {
+      this.print('WARN', ...messages)
     }
+  }
 
-    private print(level: string, ...messages: any[]) {
-        console.log(`[${new Date().toISOString()}] [${level}]`, ...messages);
-    }
+  private print (level: string, ...messages: any[]): void {
+    console.log(`[${new Date().toISOString()}] [${level}]`, ...messages)
+  }
 
-    private shouldLog(level: LogLevel): boolean {
-        const logLevels = ['WARN', 'INFO', 'DEBUG'];
-        return logLevels.indexOf(level) <= logLevels.indexOf(this.currentLogLevel);
-    }
+  private shouldLog (level: LogLevel): boolean {
+    const logLevels = ['WARN', 'INFO', 'DEBUG']
+    return logLevels.indexOf(level) <= logLevels.indexOf(this.currentLogLevel)
+  }
 }
 
-export const LOG = new Logger('DEBUG');
+export const LOG = new Logger('DEBUG')
